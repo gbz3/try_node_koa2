0831c8c27533f611bf18adf299beefcf5882f471
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-05-16 10:39:10 +0900

    pug導入

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import Koa from 'koa'
+import { Context, DefaultState } from 'koa'
 import Router from '@koa/router'
+import views from 'koa-views'
+import path from 'path'
 
 const app = new Koa()
-const router = new Router()
+const router = new Router<DefaultState, Context>()
 
 router
   .get('/', async (ctx, next) => {
     await next()
-
-    ctx.body = '{}'
-    ctx.response.set('content-type', 'application/json; charset=utf-8')
+    await ctx.render('index')
   })
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+app.use(views(`${__dirname}/../views`, { autoRender: true, extension: 'pug' }))
 app.use(router.routes())
 
 app.listen(8080, '0.0.0.0')
