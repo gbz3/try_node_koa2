710bf420a22d236bca83246a05ce4e5af32276d1
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-05-23 09:40:08 +0900

    ログカテゴリ分割

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,30 @@ import * as Router from '@koa/router'
 import * as views from 'koa-views'
 import * as JsYaml from 'js-yaml'
 import * as Fs from 'fs'
-import { configure, getLogger } from 'log4js'
+import * as Log4js from 'log4js'
 
-const logger = configure({
-  appenders:
-    { 'out': { type: 'stdout', layout: { type: 'pattern', pattern: '[%d] [%p] [%X{requestId}] : %m' } }
+const log4js = Log4js.configure({
+  appenders: {
+    'serve': { type: 'stdout', layout: { type: 'pattern', pattern: '[%d] [%p] [%X{requestId}] : %m' } },
+    'other': { type: 'stdout', layout: { type: 'pattern', pattern: '[%d] [%p] : %m' } }
   },
-  categories: { default: { appenders: ['out'], level: 'debug' } }
-}).getLogger()
+  categories: {
+    "serve": { appenders: ['serve'], level: 'debug' },
+    default: { appenders: ['other'], level: 'debug' }
+  }
+})
+const logger = log4js.getLogger()
 
 const config = JsYaml.safeLoad(Fs.readFileSync('config.yaml', { encoding: 'utf-8' }))
 const app = new Koa()
+const serveLogger = log4js.getLogger('serve')
+serveLogger.addContext('requestId', '00000000-0000-0000-0000-000000000000')
+app.context.logger = serveLogger
 const router = new Router<Koa.DefaultState, Koa.Context>()
 
 router
   .get('/', async (ctx, next) => {
+    ctx.logger.info('OK?')
     await next()
     await ctx.render('index')
   })
